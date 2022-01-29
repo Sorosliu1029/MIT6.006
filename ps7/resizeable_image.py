29c92d06864f0a108e8bfaee47f5bbc7911c2349
Soros Liu <soros.liu1029@gmail.com>
2022-01-29 13:09:47 +0800

    finish ps7 seam carving

--- ps7/resizeable_image.py
+++ ps7/resizeable_image.py
@@ -1,8 +1,42 @@
 import imagematrix
+import math
+
 
 class ResizeableImage(imagematrix.ImageMatrix):
     def best_seam(self):
-        raise NotImplemented
+        # initialize
+        dp = dict()
+        parent = dict()
+        for i in range(-1, self.width+1):
+            dp[i, -1] = 0
+        for j in range(self.height):
+            dp[-1, j] = math.inf
+            dp[self.width, j] = math.inf
+
+        # bottom-up subproblem
+        for j in range(self.height):
+            for i in range(self.width):
+                m = math.inf
+                for delta in (-1, 0, 1):
+                    if dp[i+delta, j-1] < m:
+                        m = dp[i+delta, j-1]
+                        parent[i, j] = (i+delta, j-1)
+                dp[i, j] = m + self.energy(i, j)
+
+        # original problem
+        m = math.inf
+        pos = None
+        for i in range(self.width):
+            if dp[i, self.height-1] < m:
+                pos = (i, self.height-1)
+                m = dp[pos]
+
+        coordinates = []
+        while pos in parent: 
+            coordinates.append(pos)
+            pos = parent[pos]
+
+        return list(reversed(coordinates))
 
     def remove_best_seam(self):
         self.remove_seam(self.best_seam())
--- ps7/test_resizeable_image.py
+++ ps7/test_resizeable_image.py
@@ -17,10 +17,10 @@ class TestImage(unittest.TestCase):
         self.assertEqual(image.height, len(seam), 'Seam wrong size.')
 
         # Make sure the pixels in the seam are properly ocnnected.
-        seam.sort(lambda x,y: cmp(x[1],y[1])) # Sort by height.
+        seam.sort(key=lambda pos: pos[1]) # Sort by height.
         for i in range(1, len(seam)):
-            self.assertTrue(abs(seam[i][0]-seam[i-1][0]) <= 1, 'Not a proper seam.')
             self.assertEqual(i, seam[i][1], 'Not a proper seam.')
+            self.assertTrue(abs(seam[i][0]-seam[i-1][0]) <= 1, 'Not a proper seam.')
 
         # Make sure the energy of the seam matches what we expect.
         total = sum([image.energy(coord[0], coord[1]) for coord in seam])
